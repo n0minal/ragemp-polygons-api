ae99f8e93154288cea4c0397afece8b0db52a9d4
Leonardo Moura <nominal@outlook.com.br>
2020-11-04 21:48:32 -0300

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+const { generateUniquePolygonId, getAngleSumBetweenPositionAndVertices, isPointInArea2D } = require('polygons/helpers');
+require('polygons/render');
+require('polygons/events');
+
+mp.polygons = {
+  pool: [],
+  add: (vertices, height, options = { visible: false, lineColorRGBA: [255,255,255,255], dimension: 0 }) => {
+    
+    const polygon = {
+      id: generateUniquePolygonId(),
+      vertices,
+      height,
+      ...options,
+      colliding: false
+    }
+
+    mp.polygons.pool.push(polygon);
+
+    return polygon;
+  },
+  remove: (polygon) => {
+    const index = mp.polygons.pool.findIndex(p => p.id === polygon.id);
+
+    if (index !== -1)
+      mp.polygons.pool.splice(index, 1);
+  },
+  exists: (polygon) => {
+    return mp.polygons.pool.some(p => p.id === polygon.id)
+  },
+  isPositionWithinPolygon: (position, polygon, dimension) => {
+    if (dimension && polygon.dimension !== dimension && polygon.dimension !== -1)
+      return false;
+
+    const { vertices } = polygon;
+
+    const polygonPoints2D = [];
+
+    for (let i in vertices) {
+      if (position.z >= vertices[i].z && position.z <= (vertices[i].z + polygon.height) || getAngleSumBetweenPositionAndVertices(position, vertices) >= 5.8)
+        polygonPoints2D.push([vertices[i].x, vertices[i].y]);
+      else
+        return false;  
+    }
+
+    return isPointInArea2D([position.x, position.y], polygonPoints2D);
+  }
+}
+
---
+++ render.js
@@ -0,0 +1,27 @@
+mp.events.add('render', () => {
+  
+  mp.polygons.pool?.forEach(polygon => {
+    if (!polygon.visible) return;
+
+    const { vertices, height, lineColorRGBA } = polygon;
+
+    vertices.forEach((vertex, index) => {
+      
+      const nextVertex = index  === (vertices.length - 1) ?  vertices[0] : vertices[index + 1];
+
+      // Deepness lower line
+      mp.game.graphics.drawLine(vertex.x, vertex.y, vertex.z, nextVertex.x, nextVertex.y, nextVertex.z, ...lineColorRGBA);
+      
+      // Current vertex height line
+      mp.game.graphics.drawLine(vertex.x, vertex.y, vertex.z, vertex.x, vertex.y, vertex.z + height, ...lineColorRGBA);
+      
+      // Next vertex height line
+      mp.game.graphics.drawLine(nextVertex.x, nextVertex.y, nextVertex.z, nextVertex.x, nextVertex.y, nextVertex.z + height, ...lineColorRGBA);  
+      
+      // Deepness higher line
+      mp.game.graphics.drawLine(vertex.x, vertex.y, vertex.z + height, nextVertex.x, nextVertex.y, nextVertex.z + height, ...lineColorRGBA);
+    });
+        
+  });
+
+});
